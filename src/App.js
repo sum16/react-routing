765e3b8ba2f12eecf54f701ae40da7ee7a55e4ae
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-30 01:41:16 +0900

    ネストされたページ遷移

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -13,15 +15,25 @@ export default function App() {
         <Link to="page1">page1</Link>
         <br />
         <Link to="page2">page2</Link>
-
-        {/* リンクで指定したパスが来た時にコンポーネントを振り分ける */}
+        {/* リンクで指定したパスが来た時にコンポーネントを振り分ける処理 */}
+        {/* propsの中のmatchにurl: "page1"が入っているので取り出して使う */}
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/page1">
-            <Page1 />
-          </Route>
+          <Route
+            path="/page1"
+            render={({ match: { url } }) => (
+              <Switch>
+                <Route exact path={`${url}`}>
+                  <Page1 />
+                </Route>
+                <Route path={`${url}/detailA`}>
+                  <Page1DetailA />
+                </Route>
+                <Route path={`${url}/detailB`}>
+                  <Page1DetailB />
+                </Route>
+              </Switch>
+            )}
+          />
           <Route path="/page2">
             <Page2 />
           </Route>
