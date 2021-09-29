693635812a70df2941d98b1b8340db8ec29f6de1
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-30 05:09:09 +0900

    initial commit

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   // Page2Routes.jsxで設定した:id
   const { id } = useParams();
+  const search = useLocation(); // locationのsearchにクエリパラメータが格納されている
+  // console.log(id);
+  // console.log(location);
+  const query = new URLSearchParams(search); // クエリパラメータを便利に扱えるメソッドを複数持つ
+
   return (
     <div>
       <h1>UrlParameterページ</h1>
-      <p>パラメータは{id}</p>
+      <p>URLパラメータは{id}</p>
+      <p>クエリパラメータは{query.get("name")}</p>
     </div>
   );
 };
