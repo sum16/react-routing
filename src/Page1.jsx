fa312acdccca3ecddf311834498b8be0833252a3
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-30 05:32:21 +0900

    initial commit

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
-  console.log(arr);
+  // console.log(arr);
+  const history = useHistory();
 
+  const onClickDetailA = () => {
+    history.push("page1/detailB");
+  };
   return (
     <div>
       <h1>Page1</h1>
@@ -11,6 +15,11 @@ export const Page1 = () => {
       <Link to={{ pathname: "page1/detailA", state: arr }}>ページAの詳細</Link>
       <br />
       <Link to="page1/detailB">ページBの詳細</Link>
+      <br />
+      {/* jsを使って実装 */}
+      <button onClick={onClickDetailA}>ページAの詳細</button>
     </div>
   );
 };
+
+// ボタンからリンクを飛ばしたい場合はuseHistoryを使えば良さそう
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,12 +1,17 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const location = useLocation();
-  console.log(location); // stateに渡された値が入っている (arr)
+  // console.log(location);  stateに渡された値が入っている (arr)
+  const history = useHistory();
 
+  const onClickBack = () => {
+    history.goBack(); // goBackで戻る処理
+  };
   return (
     <div>
       <h1>Page1DetailA</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
