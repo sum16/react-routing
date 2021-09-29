24f749fb10a1995afbcf89ffcf79c6532397bb6d
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-30 05:20:36 +0900

    stateを使って遷移先に値を渡す

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const location = useLocation();
+  console.log(location); // stateに渡された値が入っている (arr)
+
   return (
     <div>
       <h1>Page1DetailA</h1>
